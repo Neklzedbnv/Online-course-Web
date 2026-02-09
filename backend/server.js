44c74ad19bb06ebca002c55b3b1648327e39812c
Moldirkab <155832918+Moldirkab@users.noreply.github.com>
2026-02-09 07:42:45 +0500

    Remove server initialization code

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,1 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-const app = require("./app");
 
-mongoose
-  .connect(process.env.MONGO_URI_TEST)
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(3000, () => {
-      console.log("API running on http://localhost:3000");
-    });
-  })
-  .catch(console.error);
